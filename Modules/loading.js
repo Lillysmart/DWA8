34f22a6d238e45261c784e3fd4f2e20153a8d151
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-10-11 14:37:32 +0200

    Update loading.js

--- Modules/loading.js
+++ Modules/loading.js
@@ -1,36 +1,34 @@
 //@ts-check
 import { books, authors, genres, BOOKS_PER_PAGE } from "/data.js";
-//
+
 export let matches = books;
 export const starting = document.createDocumentFragment();
 
-for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
+/**
+ *
+ * @param {*} param0
+ */
+const createBookPreview = ({ image, title }) => {
   const element = document.createElement("button");
   element.classList = "preview";
-  element.setAttribute("data-preview", id);
+  //element.setAttribute("data-preview", id)
 
   element.innerHTML = `
         <img
             class="preview__image"
             src="${image}"
         />
-        
         <div class="preview__info">
             <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
-        </div>
-    `;
+            </div>`;
 
-  starting.appendChild(element);
-}
-
-const createPreview = ({ author, id, image, title }) => {
-  matches = {
-    get() {
-      matches.image;
-    },
-    set(value) {
-      matches.image = value;
-    },
-  };
+  return element;
 };
+const bookPreviews = matches
+  .slice(0, BOOKS_PER_PAGE)
+  .map((bookData) => createBookPreview(bookData));
+
+bookPreviews.forEach((previewElement) => {
+  starting.appendChild(previewElement);
+});
+//createPreview(book)
